198e41a4a3b5321d82b39ca5082994d3c502b3e8
Katia Ghezali <ghezalikatia@hotmail.com>
2023-09-24 16:16:37 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  
+  for(let i = 0 ; i < array.length ; i++) {
+
+    for(let j = i + 1 ; j < array.length ; j++) {
+      if((array[i] + array[j]) === target) {
+        return true
+      } 
+    }
+  }
+  return false
 }
+
+    
 
 /* 
   Write the Big O time complexity of your function here
+
+  O(n^2)
 */
 
 /* 
   Add your pseudocode here
+
+  Declare a function named hasTargetSum
+   iterate through the array
+    iterate through the array with position+1
+      if 2 values are equal
+         return true
+    return false
 */
 
 /*
   Add written explanation of your solution here
+
+
+  We go through the array twice at the same time, in a way that the loops 
+  are one position between each other. That will make it easier for us to add
+  the two values and compare their sum to the value "target" given. 
 */
 
 // You can run `node index.js` to view these console logs
